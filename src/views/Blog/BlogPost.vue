c06153ddae65bbc45aa82846a649a7dabdac9947
aminzabardast <RAZGRIZ!!stfighterjets4>
2021-07-27 17:47:45 +0300

    Working Blog Post

--- src/views/Blog/BlogPost.vue
+++ src/views/Blog/BlogPost.vue
@@ -1,25 +1,54 @@
 <template>
   <v-container class="py-0">
-    This is a post
-    {{ params }}
+    <div v-if="loading">Loading ...</div>
+    <div style="width: 100%" v-else v-html="html"></div>
   </v-container>
 </template>
 
 <script lang="js">
-import { defineComponent, ref } from '@vue/composition-api'
+import { defineComponent, ref, computed, onMounted } from '@vue/composition-api'
+import showdown from 'showdown'
+import showdownHighlight from 'showdown-highlight'
+import 'showdown-prettify'
+import axios from 'axios'
+import 'highlight.js/styles/github.css'
 
 export default defineComponent({
   name: 'BlogPost',
   setup (props, context) {
     const route = context.root.$route;
-    const params = ref(route.params)
+    const params = route.params
+    const baseUrl = process.env.BASE_URL
+    onMounted(() => {
+      const location = `${baseUrl}blog_posts/${params['year']}/${params.month}/${params.day}/${params.id}.md`
+      axios.get(location).then((data) => {
+        loading.value = false
+        rawPost.value = data.request.response
+      })
+    })
+
+    const rawPost = ref('')
+
+    showdown.setFlavor('github')
+    const converter = new showdown.Converter({
+      extensions: [
+          showdownHighlight({pre: true}), 'prettify'
+      ]
+    })
+    const html = computed(() => {
+      return converter.makeHtml(rawPost.value)
+    })
+    const loading = ref(true)
     return {
-      params
+      loading,
+      html
     }
   }
 })
 </script>
 
-<style scoped>
-
+<style>
+code.hljs {
+  width: 100% !important;
+}
 </style>
--- src/views/Blog/BlogPostList.vue
+++ src/views/Blog/BlogPostList.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="py-0">
-    <router-link to="/blog/2020/1/1/IDID">Blog</router-link>
+    <router-link to="/blog/2021/07/27/test-blog-post">Blog</router-link>
   </v-container>
 </template>
 
