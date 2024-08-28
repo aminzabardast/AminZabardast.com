a39069b5c9fcb5ebf60a602686e559baddb5e7fd
Amin Zabardast <aminzabardast@gmail.com>
2024-08-29 00:20:24 +0300

    [#30] Using flex box form styling main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,9 +7,63 @@ import Footer from '@/components/GlobalFooter.vue'
 </script>
 
 <template>
-  <WebsiteTile />
-  <HomeIntro />
-  <SocialNetworks />
-  <HomeMenu />
-  <Footer />
+  <div class="flex-container">
+    <div class="row">
+      <div class="flex-item">
+        <WebsiteTile />
+      </div>
+      <div class="flex-item">
+        <HomeIntro />
+      </div>
+      <div class="flex-item">
+        <SocialNetworks />
+      </div>
+      <div class="flex-item content-column">
+        <HomeMenu />
+      </div>
+    </div>
+  </div>
+  <div class="footer">
+    <Footer />
+  </div>
 </template>
+
+<style lang="css">
+html,
+body,
+div#app,
+main {
+  height: 100%;
+}
+body {
+  margin: 0;
+}
+.flex-container {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+}
+.flex-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.content-column {
+  flex-direction: column;
+  align-items: normal;
+}
+</style>
+
+<style lang="css" scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+}
+</style>
