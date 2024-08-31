aa6d9efb73635e92c9ab957d19fc36ff6b1f5606
Amin Zabardast <aminzabardast@gmail.com>
2024-09-01 01:37:06 +0300

    [#30] Adding colors and fonts and some redesigning

--- src/components/GlobalFooter.vue
+++ src/components/GlobalFooter.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>Footer</div>
+  <div class="footer">
+    <span> &copy; Amin Zabardast </span>
+    <span class="separator"></span>
+    <span>
+      <a class="hidden-link" href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank"
+        >CC BY-NC 4.0</a
+      >
+    </span>
+    <span class="separator"></span>
+    <span>
+      Created using
+      <a class="hidden-link" href="https://vuejs.org/" target="_blank">VueJS</a>
+    </span>
+    <span class="separator"></span>
+    <span>
+      Designed based on
+      <a class="hidden-link" href="https://github.com/Track3/hermit" target="_blank"
+        >Hermit Theme</a
+      >
+      (MIT License)
+    </span>
+  </div>
 </template>
 
 <style lang="css" scoped>
-div {
+div.footer {
   padding: 10px;
   margin: 0px;
-  background-color: gold;
+  text-align: center;
+  font-size: 10px;
+  color: #7c7c7c;
+}
+
+.separator::after {
+  content: ' | ';
+  padding: 0 5px;
 }
 </style>
