9aaca1d6b337ec8ea61e6d7fa81611d75dbe9628
Amin Zabardast <aminzabardast@gmail.com>
2024-09-01 19:05:36 +0300

    [#30] Used the new link in the footer

--- src/components/GlobalFooter.vue
+++ src/components/GlobalFooter.vue
@@ -3,37 +3,45 @@
     <span> &copy; Amin Zabardast </span>
     <span class="separator"></span>
     <span>
-      <a class="hidden-link" href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank"
-        >CC BY-NC 4.0</a
+      <Link color="#EF9421" href="https://creativecommons.org/licenses/by-nc/4.0/" target="_blank"
+        >CC BY-NC 4.0</Link
       >
     </span>
     <span class="separator"></span>
     <span>
       Created using
-      <a class="hidden-link" href="https://vuejs.org/" target="_blank">VueJS</a>
+      <Link color="#4FC08D" href="https://vuejs.org/" target="_blank">VueJS</Link>
     </span>
     <span class="separator"></span>
     <span>
       Designed based on
-      <a class="hidden-link" href="https://github.com/Track3/hermit" target="_blank"
-        >Hermit Theme</a
+      <Link color="lightgrey" href="https://github.com/Track3/hermit" target="_blank"
+        >Hermit Theme</Link
       >
       (MIT License)
     </span>
   </div>
 </template>
 
+<script lang="ts" setup>
+import Link from '@/components/AnimatedLink.vue'
+</script>
+
 <style lang="css" scoped>
 div.footer {
   padding: 10px;
   margin: 0px;
   text-align: center;
-  font-size: 10px;
+  font-size: 0.8em;
   color: #7c7c7c;
 }
 
 .separator::after {
   content: ' | ';
   padding: 0 5px;
+}
+
+a {
+  color: #7c7c7c;
 }
 </style>
