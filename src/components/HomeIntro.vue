7d36fb64429b16233b203790b17110043943ea7a
Amin Zabardast <aminzabardast@gmail.com>
2024-09-01 19:05:04 +0300

    [#30] Changed Intro Desc

--- src/components/HomeIntro.vue
+++ src/components/HomeIntro.vue
@@ -1,7 +1,7 @@
 <template>
   <p>
-    This is a medium medium medium medium medium medium medium medium medium medium length
-    explanation.
+    Data Scientist, Software Developer,<br />
+    Creative Problem Solver, Tech Enthusiast
   </p>
 </template>
 
@@ -11,7 +11,7 @@ p {
   margin: 0;
   text-align: center;
   font-style: italic;
-  font-size: 1.1em;
-  letter-spacing: 0.06em;
+  font-size: 1em;
+  letter-spacing: 0.1em;
 }
 </style>
