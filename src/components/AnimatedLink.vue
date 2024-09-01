76e0d083de2aeffd4f0aef4a670d1455e364dab0
Amin Zabardast <aminzabardast@gmail.com>
2024-09-01 19:04:00 +0300

    [#30] Added link component

---
+++ src/components/AnimatedLink.vue
@@ -0,0 +1,61 @@
+<template>
+  <a :href="props.href" :target="props.target">
+    <span></span>
+    <div>
+      <slot></slot>
+    </div>
+  </a>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+export interface Props {
+  href?: string
+  color: string
+  target?: '_blank' | ''
+  underline?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  target: '',
+  mode: '',
+  underline: false
+})
+
+const spanHeight = computed(() => {
+  return props.underline ? '8px' : '0'
+})
+</script>
+
+<style lang="css" scoped>
+a {
+  position: relative;
+  bottom: 0;
+}
+
+a div {
+  display: inline-block;
+  position: relative;
+  z-index: 2;
+  height: 100%;
+}
+
+span {
+  z-index: 1;
+  position: absolute;
+  height: v-bind('spanHeight');
+  width: 100%;
+  background-color: v-bind('$props.color');
+  bottom: 0;
+  left: 0;
+  -webkit-transition: height 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  -moz-transition: height 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  -o-transition: height 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  transition: height 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+a:hover span {
+  height: 100%;
+}
+</style>
