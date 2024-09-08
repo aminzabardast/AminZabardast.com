cc5d0fbc0120a5ca3d6f0e6ffbb2562cfda87d3b
Amin Zabardast <aminzabardast@gmail.com>
2024-09-08 04:26:37 +0300

    [#47] Year Component

---
+++ src/components/MediumYear.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3><slot></slot></h3>
+</template>
+
+<script lang="ts" setup>
+export interface Props {
+  published: Date
+  url: string
+}
+</script>
+
+<style lang="css" scoped>
+h3 {
+  font-family: 'Unica One', sans-serif;
+  padding: 10px;
+  margin: 0;
+}
+</style>
