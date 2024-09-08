8e6717ab8d7095591a51ee973d5ab558781dcb1d
Amin Zabardast <aminzabardast@gmail.com>
2024-09-09 01:04:34 +0300

    [#63] Unsplash Views are added

---
+++ src/views/UnsplashPopularView.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- TODO: Title of page is missing -->
+  <!-- TODO: Link to home is missing -->
+  <div class="loading-div" v-if="loading">
+    <LoadingSpinner />
+  </div>
+  <ImageGallery :images="listOfImages" />
+  <!-- TODO: More on Unsplash is missing -->
+</template>
+
+<script lang="ts" setup>
+import ImageGallery, { type Image } from '@/components/ImageGallery.vue'
+import { ref, onMounted, type Ref } from 'vue'
+import { get, map } from 'lodash'
+import LoadingSpinner from '@/components/LoadingSpinner.vue'
+
+const fetchFeed = async () => {
+  const apiUrl = ''
+  const response = await fetch(`${apiUrl}/api/v1/unsplash/popular/`)
+  const jsonResponse = await response.json()
+  return jsonResponse as JSON
+}
+
+const listOfImages = ref([]) as Ref<Image[]>
+const loading = ref(false)
+
+onMounted(() => {
+  loading.value = true
+  fetchFeed()
+    .then((data) => {
+      const processedData = map(data, (item): Image => {
+        console.log(item)
+        return {
+          imageUrl: get(item, 'urls.small'),
+          url: get(item, 'links.html'),
+          color: get(item, 'color')
+        }
+      })
+      listOfImages.value = processedData
+    })
+    .finally(() => {
+      loading.value = false
+    })
+})
+</script>
+
+<style lang="css" scoped>
+.loading-div > div {
+  margin: auto;
+}
+</style>
